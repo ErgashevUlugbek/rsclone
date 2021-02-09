0faaf8ac845060e030a6614d76c5d6a3b8021af9
Ergashev Ulug`bek <eulugbek2000@gmail.com>
2021-02-09 08:19:56 +0500

    fix: fix header style

--- src/components/common/common.ts
+++ src/components/common/common.ts
@@ -1,18 +1,20 @@
 export default function commonTags() {
     const header: HTMLElement = document.createElement("header");
     const main: HTMLElement = document.createElement("main");
-    const footer: HTMLElement = document.createElement("footer")
+    const footer: HTMLElement = document.createElement("footer");
     header.innerHTML = `
-        <div class="courses">courses</div>
-        <div class="search">
-            <div class="form-group d-flex">
-                <input type="text" class="form-control rounded-end-0" id="search" placeholder="Search" value="hello">
-                <label for="search">
-                    <button class="btn btn-dark text-white rounded-start-0" id="search-button">Search</button>
-                </label>
+        <div class="container">
+            <div class="courses">courses</div>
+            <div class="search">
+                <div class="form-group d-flex">
+                    <input type="text" class="form-control rounded-end-0" id="search" placeholder="Search" value="hello">
+                    <label for="search">
+                        <button class="btn btn-dark text-white rounded-start-0" id="search-button">Search</button>
+                    </label>
+                </div>
             </div>
+            <div class="notification"></div>
         </div>
-        <div class="notification"></div>
     `;
 
     main.innerHTML = `
@@ -22,14 +24,12 @@ export default function commonTags() {
     `;
 
     footer.innerHTML = `
-        <footer>
-            <div class="d-flex">
-                <div>Tab 1</div>
-                <div>Tab 2</div>
-                <div>Tab 3</div>
-                <div>Tab 4</div>
-            </div>
-        </footer>
+        <div class="container">
+            <div>Tab 1</div>
+            <div>Tab 2</div>
+            <div>Tab 3</div>
+            <div>Tab 4</div>
+        </div>
     `
 
     return [header, main, footer];
